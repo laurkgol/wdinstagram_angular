f3721f3c1e2cc329bda16c608a817562edfa0a5a
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-07 21:52:08 -0500

    working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,91 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+
+  angular
+  .module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ])
+    .factory("instaFactory", [
+      "$resource",
+      instaFactoryFunction
+    ])
+    .controller("instaIndexController", [
+      "instaFactory",
+      "$stateParams",
+      instaIndexControllerFunction
+    ])
+    .controller( "instaNewController", [
+      "instaFactory",
+      instaNewControllerFunction
+    ])
+    .controller("instaShowController", [
+      "instaFactory",
+      "$stateParams",
+      instaShowControllerFunction
+    ])
+    .controller("instaEditController", [
+      "instaFactory",
+      "$stateParams",
+      instaEditControllerFunction
+    ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("instaIndex", {
+      url: "/instas",
+      templateUrl: "js/ng-views/index.html",
+      controller: "instaIndexController",
+      controllerAs: "vm"
+    })
+    .state("instaNew", {
+      url: "/instas/new",
+      templateUrl: "js/ng-views/new.html",
+      controller: "instaNewController",
+      controllerAs: "vm"
+    })
+    .state("instaShow", {
+      url: "/instas/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "instaShowController",
+      controllerAs: "vm"
+    })
+    .state("instaEdit", {
+      url: "/instas/:id/edit",
+      templateUrl: "js/ng-views/edit.html",
+      controller: "instaEditController",
+      controllerAs: "vm"
+    })
+  };
+
+  function instaFactoryFunction( $resource ) {
+    return $resource("http://localhost:3000/entries/:id", {}, {
+      update: {method: "PUT"}
+    })
+  }
+
+  function instaIndexControllerFunction( instaFactory ){
+    this.instas = instaFactory.query();
+  }
+
+  function instaNewControllerFunction( instaFactory ){
+      this.insta = new instaFactory();
+      this.create = function(){
+        this.insta.$save()
+      }
+    }
+
+  function instaShowControllerFunction( instaFactory, $stateParams ){
+    this.insta = instaFactory.get({id: $stateParams.id});
+  }
+
+  function instaEditControllerFunction(instaFactory, $stateParams){
+    this.insta = instaFactory.get({id: $stateParams.id})
+    this.update = function(){
+      this.insta.$update({id: $stateParams})
+    }
+}
